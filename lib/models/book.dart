9b02bc55a43218e0dceb64c24311cbca32a4b885
Alex Borges <alexborgesramos2002@gmail.com>
2023-02-12 14:13:11 -0300

    Update Book model

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,25 +1,36 @@
 import 'dart:convert';
 
+import 'package:anr/models/scan.dart';
 import 'package:slugify/slugify.dart';
 
 class Book {
-  const Book({required this.name, required this.path, required this.src, this.webID, this.type});
+  const Book({required this.name, required this.path, required this.src, required this.scan, this.type, this.webID});
 
+  final Scan scan;
   final String name;
   final String path;
   final String src;
 
+  final String? type;
   final int? webID;
-  final int? type;
 
   String get slug => slugify(name.trim(), lowercase: true, delimiter: '_');
 
-  Map<String, dynamic> toMap() => {'name': name, 'path': path, 'src': src, 'webID': webID, 'type': type};
+  Map<String, dynamic> toMap() {
+    return {'name': name, 'path': path, 'src': src, 'scan': scan.value, 'webID': webID, 'type': type};
+  }
 
   String toJson() => json.encode(toMap());
 
   factory Book.fromMap(Map<String, dynamic> map) {
-    return Book(name: map['name'], path: map['path'], src: map['src'], webID: map['webID'], type: map['type']);
+    return Book(
+      src: map['src'],
+      name: map['name'],
+      path: map['path'],
+      scan: map['scan'] is String ? scanByValue(map['scan']) : map['scan'],
+      type: map['type'],
+      webID: map['webID'],
+    );
   }
 
   factory Book.fromJson(String source) => Book.fromMap(json.decode(source) as Map<String, dynamic>);
