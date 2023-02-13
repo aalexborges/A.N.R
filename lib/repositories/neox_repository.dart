35561d82440cb6dbfc5c281c8d79688a28081c22
Alex Borges <alexborgesramos2002@gmail.com>
2023-02-13 17:57:58 -0300

    Remove package dio_http_cache

--- lib/repositories/neox_repository.dart
+++ lib/repositories/neox_repository.dart
@@ -15,7 +15,7 @@ class NeoxRepository extends ScanBaseRepository {
       callback: (baseURL) async {
         final books = <Book>[];
 
-        final response = await dio(baseURL).get(baseURL, options: cacheOptions());
+        final response = await dio(baseURL).get(baseURL);
         final $ = parse(response.data);
 
         for (Element element in $.querySelectorAll('#loop-content .row div.page-item-detail')) {
@@ -52,7 +52,7 @@ class NeoxRepository extends ScanBaseRepository {
         final subKey = '?s=$value&post_type=wp-manga';
         final url = '$baseURL/$subKey';
 
-        final response = await dio(baseURL).get(url, options: cacheOptions(subKey: subKey));
+        final response = await dio(baseURL).get(url);
         final $ = parse(response.data);
 
         for (Element element in $.querySelectorAll('.c-tabs-item div.row')) {
--- lib/repositories/scan_base_repository.dart
+++ lib/repositories/scan_base_repository.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 
 import 'package:anr/models/book.dart';
+import 'package:anr/models/book_data.dart';
 import 'package:anr/models/scan.dart';
 import 'package:anr/utils/scraping_util.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
-import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:html/dom.dart';
 import 'package:html/parser.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -22,27 +22,15 @@ abstract class ScanBaseRepository {
 
   Future<List<Book>> search(String value) async => [];
 
+  Future<BookData> data(Book book) async => const BookData(sinopse: '', chapters: [], categories: []);
+
   Dio dio(String baseURL) {
     final client = Dio(BaseOptions(headers: headers));
     final cookieJar = CookieJar();
-    final dioCacheManager = DioCacheManager(CacheConfig(baseUrl: baseURL));
 
-    client.interceptors.addAll([dioCacheManager.interceptor, PrettyDioLogger(), CookieManager(cookieJar)]);
+    client.interceptors.addAll([PrettyDioLogger(), CookieManager(cookieJar)]);
 
     return client;
-  }
-
-  Options cacheOptions({String? subKey, Options? options, bool? forceRefresh = true}) {
-    return buildCacheOptions(
-      const Duration(days: 31),
-      subKey: subKey,
-      options: options,
-      forceRefresh: forceRefresh,
-    );
-  }
-
-  bool _isCacheResponse(Response response) {
-    return null != response.headers.value(DIO_CACHE_HEADER_KEY_DATA_SOURCE);
   }
 
   Future<T> _tryAllURLs<T>({required Future<T> Function(String baseURL) callback, T? defaultValue}) async {
