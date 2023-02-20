cc153b63d729857d26720f6526892bbd3bc60302
Alex Borges <alexborgesramos2002@gmail.com>
2023-02-20 16:54:16 -0300

    Update FavoritesRepository

--- lib/repositories/favorites_repository.dart
+++ lib/repositories/favorites_repository.dart
@@ -1,3 +1,4 @@
+import 'package:anr/models/book.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -5,13 +6,30 @@ class FavoritesRepository {
   FavoritesRepository._() {
     final user = FirebaseAuth.instance.currentUser;
 
-    if (user is User) _ref = FirebaseDatabase.instance.ref('users/${user.uid}/favorites')..keepSynced(true);
+    if (user is User) {
+      _ref = FirebaseDatabase.instance.ref('users/${user.uid}/favorites');
+      _ref!.keepSynced(true);
+    }
   }
 
-  static final instance = FavoritesRepository._();
-  static late DatabaseReference _ref;
+  static final I = FavoritesRepository._();
+  static DatabaseReference? _ref;
 
-  get all async {
-    final snapshot = await _ref.get();
+  Future<bool> isFavorite(String slug) async {
+    final ref = _ref;
+
+    if (ref == null) return false;
+
+    final data = await ref.child(slug).get();
+    return data.exists;
+  }
+
+  Future<void> setFavorite({required Book book, bool? remove}) async {
+    final ref = _ref;
+
+    if (ref == null) throw Error();
+
+    if (remove == true) return await ref.child(book.slug).remove();
+    await ref.child(book.slug).set(book.toMap());
   }
 }
