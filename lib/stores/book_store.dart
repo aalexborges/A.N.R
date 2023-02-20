31e3a15607fc0ecab73fc6cd86b5f8a6303fdc0f
Alex Borges <alexborgesramos2002@gmail.com>
2023-02-20 16:55:13 -0300

    Create BookStore

---
+++ lib/stores/book_store.dart
@@ -0,0 +1,44 @@
+import 'package:anr/models/book.dart';
+import 'package:anr/models/book_data.dart';
+import 'package:anr/models/order.dart';
+import 'package:anr/models/scan.dart';
+import 'package:mobx/mobx.dart';
+
+part 'book_store.g.dart';
+
+// ignore: library_private_types_in_public_api
+class BookStore = _BookStore with _$BookStore;
+
+abstract class _BookStore with Store {
+  @observable
+  BookData? data;
+
+  @observable
+  bool pinnedTitle = false;
+
+  @observable
+  Order order = Order.desc;
+
+  @computed
+  List get chapters {
+    if (data == null) return List.empty();
+    if (order == Order.desc) return data!.chapters;
+
+    return List.from(data!.chapters, growable: false).reversed.toList();
+  }
+
+  @action
+  Future<void> getData(Book book) async {
+    data = await book.scan.repository.data(book);
+  }
+
+  @action
+  void setPinnedTitle(bool value) {
+    pinnedTitle = value;
+  }
+
+  @action
+  void toggleOrder() {
+    order = order == Order.desc ? Order.asc : Order.desc;
+  }
+}
