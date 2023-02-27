7da34089f2ce04eb715d63f3cd84604954b0eaf9
Alex Borges <alexborgesramos2002@gmail.com>
2023-02-27 19:37:48 -0300

    Update ChapterListItem

--- lib/widgets/chapter_list_item.dart
+++ lib/widgets/chapter_list_item.dart
@@ -1,6 +1,11 @@
+import 'package:anr/models/book.dart';
 import 'package:anr/models/chapter.dart';
+import 'package:anr/models/content.dart';
+import 'package:anr/models/order.dart';
 import 'package:anr/repositories/reading_history_repository.dart';
+import 'package:anr/router.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ChapterListItem extends StatelessWidget {
   const ChapterListItem({super.key, required this.chapter, this.onTap});
@@ -14,6 +19,23 @@ class ChapterListItem extends StatelessWidget {
       title: Text(chapter.name),
       trailing: ReadingHistoryRepository.I.progressWidget(chapter),
       onTap: onTap,
+    );
+  }
+
+  static toDetails({
+    required BuildContext context,
+    required Book book,
+    required Order order,
+    required List<Chapter> chapters,
+    required int startAt,
+  }) {
+    context.push(
+      ScreenPaths.content,
+      extra: Content(
+        book: book,
+        chapters: chapters,
+        startAt: Content.startByOrder(startAt, order, chapters.length),
+      ),
     );
   }
 }
