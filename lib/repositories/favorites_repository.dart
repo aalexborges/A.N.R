ac941e7c31dc9520d47e23473f2976a92ae440d6
Alex Borges <alexborgesramos2002@gmail.com>
2023-03-12 18:05:38 -0300

    Update FavoritesRepository

--- lib/repositories/favorites_repository.dart
+++ lib/repositories/favorites_repository.dart
@@ -3,31 +3,40 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class FavoritesRepository {
-  FavoritesRepository._() {
+  const FavoritesRepository._();
+
+  static DatabaseReference? _baseRef;
+  static const I = FavoritesRepository._();
+
+  Future<DatabaseReference> get baseRef async {
+    if (_baseRef is DatabaseReference) return _baseRef!;
+
+    _baseRef = await _initBaseRef();
+    return _baseRef!;
+  }
+
+  Future<DatabaseReference> _initBaseRef() async {
     final user = FirebaseAuth.instance.currentUser;
 
     if (user is User) {
-      _ref = FirebaseDatabase.instance.ref('users/${user.uid}/favorites');
-      _ref!.keepSynced(true);
+      final ref = FirebaseDatabase.instance.ref('users/${user.uid}/favorites');
+      await ref.keepSynced(true);
+
+      return ref;
     }
+
+    throw Error();
   }
 
-  static final I = FavoritesRepository._();
-  static DatabaseReference? _ref;
-
   Future<bool> isFavorite(String slug) async {
-    final ref = _ref;
-
-    if (ref == null) return false;
-
+    final ref = await baseRef;
     final data = await ref.child(slug).get();
+
     return data.exists;
   }
 
   Future<void> setFavorite({required Book book, bool? remove}) async {
-    final ref = _ref;
-
-    if (ref == null) throw Error();
+    final ref = await baseRef;
 
     if (remove == true) return await ref.child(book.slug).remove();
     await ref.child(book.slug).set(book.toMap());
