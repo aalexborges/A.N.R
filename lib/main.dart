1edf942edff07bbe807fe0bc30d3ca3670ad75b6
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-07-16 17:29:50 -0300

    Updates main and I10n

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:anr/firebase_options.dart';
 import 'package:anr/routes.dart';
 import 'package:anr/service_locator.dart';
+import 'package:anr/services/theme_service.dart';
 import 'package:anr/utils/color_schemes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
@@ -28,10 +30,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = GetIt.I.get<ThemeService>().getThemeMode();
+
+  void _themeListening() {
+    setState(() {
+      _themeMode = GetIt.I.get<ThemeService>().getThemeMode();
+    });
+  }
+
   @override
   void initState() {
     FlutterNativeSplash.remove();
+    themeService.addListener(_themeListening);
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    themeService.removeListener(_themeListening);
+
+    super.dispose();
   }
 
   @override
@@ -40,6 +59,7 @@ class _MyAppState extends State<MyApp> {
       title: 'A.N.R',
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      themeMode: _themeMode,
       routerDelegate: AppRouter.router.routerDelegate,
       routeInformationParser: AppRouter.router.routeInformationParser,
       routeInformationProvider: AppRouter.router.routeInformationProvider,
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,7 +1,12 @@
+import 'package:anr/services/authentication_service.dart';
 import 'package:anr/services/theme_service.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void setupServiceLocator(SharedPreferences prefs) {
   GetIt.I.registerLazySingleton<ThemeService>(() => ThemeService(prefs));
+  GetIt.I.registerLazySingleton<AuthenticationService>(() => AuthenticationService());
 }
+
+ThemeService get themeService => GetIt.I.get<ThemeService>();
+AuthenticationService get authenticationService => GetIt.I.get<AuthenticationService>();
