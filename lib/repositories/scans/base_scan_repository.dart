920ba855612bc6ada1cbc1f8bfa84de145ca40d4
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-08-22 17:55:34 -0300

    Creates abstract BaseScanRepository

---
+++ lib/repositories/scans/base_scan_repository.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:anr/models/book_item.dart';
+import 'package:anr/models/scan.dart';
+import 'package:anr/service_locator.dart';
+import 'package:anr/utils/scraping_utils.dart';
+import 'package:html/parser.dart';
+
+part 'package:anr/repositories/scans/hunters_repository.dart';
+part 'package:anr/repositories/scans/manga_host_repository.dart';
+part 'package:anr/repositories/scans/manga_livre_repository.dart';
+part 'package:anr/repositories/scans/neox_repository.dart';
+part 'package:anr/repositories/scans/prisma_repository.dart';
+part 'package:anr/repositories/scans/random_repository.dart';
+part 'package:anr/repositories/scans/reaper_repository.dart';
+
+abstract class BaseScanRepository {
+  final Map<String, String>? headers = null;
+
+  Future<List<BookItem>> lastAdded({forceUpdate = false}) async => [];
+  Future<List<BookItem>> search(String value, {bool forceUpdate = false}) async => [];
+}
