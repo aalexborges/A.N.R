acf9e7b9ed621dbaf6020ae9ca4a335a9125eaa3
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-08-23 19:01:19 -0300

    Updates routes path

--- lib/utils/route_paths.dart
+++ lib/utils/route_paths.dart
@@ -3,5 +3,6 @@ class RoutePaths {
   static const String home = '/home';
   static const String login = '/login';
   static const String search = '/search';
+  static const String details = '/details';
   static const String favorites = '/favorites';
 }
