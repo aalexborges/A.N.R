5c7b1967db552862be338a7f6302f0d639955265
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-08-27 12:38:49 -0300

    Updates RoutePaths

--- lib/utils/route_paths.dart
+++ lib/utils/route_paths.dart
@@ -2,6 +2,7 @@ class RoutePaths {
   static const String book = '/book';
   static const String home = '/home';
   static const String login = '/login';
+  static const String reader = '/reader';
   static const String search = '/search';
   static const String details = '/details';
   static const String favorites = '/favorites';
